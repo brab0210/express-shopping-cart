d3ff35d0f468be81660fe283394ca2b94a3d863f
digitalhedu <evirgilio@digitalhouse.com>
2022-04-05 19:49:36 -0300

    Starting Code

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
 app.set('port', (process.env.PORT || 3030));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.listen(app.get('port'), () => {
+  console.log('Server started on port http://localhost:' + app.get('port'));
+});
+
+app.use(require('./routes/main'));
